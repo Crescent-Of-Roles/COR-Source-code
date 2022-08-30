98a3a47ad29943fc4027c6d0c691d865ec132bc3
olive <107787593+mikan956@users.noreply.github.com>
2022-08-30 16:20:56 +0900

    Add files via upload

---
+++ COR/CORplugin/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+
+namespace CORplugin
+{
+    [BepInPlugin("create by,olive", "CORplugin", "1,00")]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo("Plugin {CrescentOfRoles} is loaded!");
+        }
+    }
+}
+//このPlugin.csはオリーブが書きました
---
+++ COR/CORplugin/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CORplugin
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "CORplugin";
+        public const string PLUGIN_NAME = "CORplugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
