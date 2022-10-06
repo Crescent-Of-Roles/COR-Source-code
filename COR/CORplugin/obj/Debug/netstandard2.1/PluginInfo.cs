cfee9b79842ddc946c20141b8ed50f0f642cdd59
olive <mikanbeya@gmail.com>
2022-10-06 11:50:51 +0900

    簡略化 ファイル削除等々

--- COR/CORplugin/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CORplugin
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CORplugin";
-        public const string PLUGIN_NAME = "CORplugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
